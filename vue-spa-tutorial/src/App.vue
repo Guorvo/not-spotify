5970307dcdb8dc2ffc9f4832327427eb78a80c00
Guorvo <lmcuchero@gmail.com>
2023-08-28 15:34:03 -0300

    first batch of vue tutorials

---
+++ vue-spa-tutorial/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import DeclarativeRendering from './components/DeclarativeRendering.vue'
+  import AttributeBindings from './components/AttributeBindings.vue'
+</script>
+
+<template>
+  <DeclarativeRendering/>
+  <AttributeBindings/>
+</template>
---
+++ vue-spa-tutorial/src/components/EventListeners.vue
@@ -0,0 +1,24 @@
+<!-- <script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  make this button work
+  <button>count is: {{ count }}</button>
+</template> -->
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">count is: {{ count }}</button>
+</template>
