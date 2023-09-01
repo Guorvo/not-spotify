82f93d48448d37d4d7bf66ced4f0561f28df1d3e
Guorvo <lmcuchero@gmail.com>
2023-09-01 17:47:16 -0300

    removed github source error

--- vue-spa-tutorial/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-  import DeclarativeRendering from './components/DeclarativeRendering.vue'
-  import AttributeBindings from './components/AttributeBindings.vue'
-</script>
-
-<template>
-  <DeclarativeRendering/>
-  <AttributeBindings/>
-</template>
